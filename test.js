48948809a695dd8e30a8ca94afe28a0cf5cb1f18
Gokul Krishna <amkrishna2483@gmail.com>
2024-02-11 19:08:08 +0530

    logs and deletes

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 const sortedArray = [1, 12, 23, 34, 45, 56, 67, 78, 89, 90];
 
 const unsortedArray = [23,12,45,23,67,3,67,98,1,23,12,37,77];
+
+
+//console.log(sortedArray)
+//console.log(unsortedArray)
