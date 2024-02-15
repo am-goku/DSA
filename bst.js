7eb95b9a6bc7fa0564a7c60f8bf95161b2157d2e
Gokul Krishna <amkrishna2483@gmail.com>
2024-02-15 17:22:53 +0530

    graph updated

--- bst.js
+++ bst.js
@@ -1,4 +1,6 @@
 
+
+/* The treeNode class represents a node in a binary tree with a value, a left child, and a right child. */
 class treeNode {
   constructor(value){
     this.value = value;
@@ -8,12 +10,23 @@ class treeNode {
 }
 
 
+/* The BinaryTree class represents a binary search tree and provides methods for inserting, deleting,
+searching, and traversing the tree. */
 class BinaryTree{
+
   constructor(){
     this.root = null;
   }
 
 
+  /**
+   * The insert function adds a new node with a given value to a binary search tree.
+   * @param value - The value parameter represents the value that you want to insert into the binary
+   * search tree.
+   * @returns In this code snippet, nothing is being explicitly returned. However, if the `insert`
+   * function is being called as a method on an object, it is likely that the object itself is being
+   * implicitly returned.
+   */
   insert(value){
     const newNode = new treeNode(value);
 
@@ -43,6 +56,13 @@ class BinaryTree{
 
 
 
+  
+  /**
+   * The inOrder function recursively traverses a binary tree and prints the values of each node in
+   * ascending order.
+   * @param [node] - The parameter "node" represents the current node being visited in a binary tree
+   * traversal. It is initially set to the root node of the tree.
+   */
   inOrder(node = this.root){
     if(node){
       this.inOrder(node.left);
@@ -52,6 +72,11 @@ class BinaryTree{
   }
 
 
+  /**
+   * The postOrder function recursively traverses a binary tree and prints the value of each node in
+   * post-order.
+   * @param [node] - The "node" parameter represents the current node being traversed in a binary tree.
+   */
   postOrder(node = this.root){
     if(node){
       console.log(node.value);
@@ -61,6 +86,11 @@ class BinaryTree{
   }
 
 
+  /**
+   * The preOrder function recursively traverses a binary tree and prints the value of each node in
+   * post-order.
+   * @param [node] - The parameter "node" represents the current node being visited in a binary tree.
+   */
   preOrder(node = this.root){
     if(node){
       this.preOrder(node.left);
@@ -70,11 +100,25 @@ class BinaryTree{
   }
 
 
+  /**
+   * The delete function is used to remove a node with a specific value from a binary search tree.
+   * @param value - The value parameter represents the value of the node that needs to be deleted from
+   * the binary search tree.
+   */
   delete(value){
     this.root = this.deleteHelper(this.root, value);
   }
 
 
+  /**
+   * The function `deleteHelper` is a recursive function that deletes a node with a given key from a
+   * binary search tree.
+   * @param root - The root parameter represents the root node of a binary search tree.
+   * @param key - The `key` parameter in the `deleteHelper` function represents the value that needs to
+   * be deleted from the binary search tree.
+   * @returns the modified root node of the binary search tree after deleting the node with the
+   * specified key.
+   */
   deleteHelper(root, key){
     if(!root){
       return null
@@ -100,6 +144,11 @@ class BinaryTree{
   }
 
 
+  /**
+   * The function finds the minimum value in a binary tree by traversing to the leftmost node.
+   * @param node - The parameter "node" represents a node in a binary search tree.
+   * @returns The minimum value node in the given binary tree.
+   */
   findMin(node){
     while(node.left){
       node = node.left
@@ -109,6 +158,13 @@ class BinaryTree{
   }
 
 
+  /**
+   * The function searches for a specific value in a binary search tree and returns true if the value
+   * is found, otherwise it returns false.
+   * @param key - The key parameter is the value that we are searching for in the binary search tree.
+   * @returns a boolean value. It returns true if the key is found in the binary search tree, and false
+   * otherwise.
+   */
   search(key) {
     let current = this.root;
 
@@ -126,6 +182,9 @@ class BinaryTree{
   }
 
 
+  /**
+   * The `deleteDuplicates` function deletes duplicate elements from an array.
+   */
   deleteDuplicates(){
     const data = [], duplicates = [];
 
@@ -137,6 +196,17 @@ class BinaryTree{
   }
 
 
+  /**
+   * The function `findDuplicates` recursively searches a binary tree and adds any duplicate values to
+   * the `duplicates` array.
+   * @param duplicates - The "duplicates" parameter is an array that will store any duplicate values
+   * found in the "data" array.
+   * @param data - The `data` parameter is an array that contains the values of nodes in a binary tree.
+   * @param [node] - The "node" parameter represents the current node being processed in a binary tree.
+   * @returns There is no explicit return statement in the code provided. However, the function is
+   * being called recursively, so it is likely that the function is being used to modify the
+   * "duplicates" and "data" arrays passed as arguments.
+   */
   findDuplicates(duplicates, data, node = this.root){
     if(node){
       this.findDuplicates(duplicates, data, node.left);
@@ -152,6 +222,13 @@ class BinaryTree{
     }
   }
 
+
+  /**
+   * The function finds the height of a binary tree by recursively calculating the maximum height of
+   * its left and right subtrees.
+   * @param [root] - The "root" parameter represents the root node of a binary tree.
+   * @returns The height of the binary tree rooted at the given root node.
+   */
   findHeight(root = this.root) {
     if(!root) return -1
 
@@ -165,6 +242,8 @@ class BinaryTree{
 
 
 
+/* The code snippet is creating a binary search tree using the `BinaryTree` class and performing
+various operations on it. */
 const tree = new BinaryTree();
 
 tree.insert(10);
@@ -179,7 +258,7 @@ tree.insert(5);
 
 tree.inOrder()
 tree.preOrder()
-tree.postOrder();
+tree.postOrder()
 
 tree.deleteDuplicates() 
 
@@ -187,6 +266,6 @@ tree.delete(40)
 
 console.log("Height og the tree is: ", tree.findHeight())
 
-console.log(tree.search(10), tree.search(100));
+console.log("Search results: ",tree.search(10), tree.search(100));
 
-console.log(tree.root);
+console.log("Tree: ", tree.root);
--- graph.js
+++ graph.js
@@ -1,69 +1,111 @@
+/* The GraphNode class represents a graph data structure and provides methods for adding vertices and
+edges, as well as performing depth-first and breadth-first search traversals. */
 class GraphNode{
-  constructor() {
+  /**
+   * The constructor function initializes an empty adjacency list using a Map data structure.
+   */
+  constructor(){
     this.adjacencyList = new Map();
   }
 
-  addvertex(vertex) {
-    if(!this.adjacencyList.has(vertex)){
-      this.adjacencyList.set(vertex, [])
+
+  /**
+   * The function adds a vertex to the adjacency list if it does not already exist.
+   * @param value - The value parameter represents the value or identifier of the vertex that you want
+   * to add to the graph.
+   */
+  addvertex(value) {
+    if(!this.adjacencyList.has(value)){
+      this.adjacencyList.set(value, [])
     }
   }
 
-  addEdges(v1, v2) {
-    this.adjacencyList.get(v1).push(v2);
+  /**
+   * The function adds an edge between two vertices in an adjacency list.
+   * @param v1 - The first vertex of the edge.
+   * @param v2 - The parameter "v2" represents the second vertex of the edge that you want to add to
+   * the graph.
+   */
+  addEdges(v1, v2){
+    this.adjacencyList.get(v1).push(v2)
     this.adjacencyList.get(v2).push(v1);
   }
 
-  dfsTravel(startingVertex) {
+
+  /**
+   * The function performs a depth-first search traversal starting from a given vertex and returns the
+   * visited vertices in the order they were visited.
+   * @param startingVertex - The startingVertex parameter is the vertex from which the depth-first
+   * search traversal will begin. It is the vertex where the traversal will start exploring the graph.
+   * @returns an array containing the vertices visited during the Depth First Search traversal starting
+   * from the given startingVertex.
+   */
+  dfsTravel(startingVertex){
     const visited = new Set();
+
     const result = [];
 
     const dfs = (vertex) => {
       visited.add(vertex);
-      result.push(vertex);
+      result.push(vertex)
 
-      const current = this.adjacencyList.get(vertex)
+      const currVertex = this.adjacencyList.get(vertex);
 
-      current.forEach(node => {
-        if(!visited.has(node)){
-          dfs(node)
+      currVertex.forEach(e => {
+        if(!visited.has(e)){
+          dfs(e)
         }
-      });
+      })
     }
 
     dfs(startingVertex);
+
+    return result;
+
+  }
+
+
+
+  /**
+   * The bfs function performs a breadth-first search starting from a given vertex and returns the
+   * visited vertices in the order they were visited.
+   * @param startingVertex - The startingVertex parameter is the vertex from which the breadth-first
+   * search (BFS) will begin. It is the vertex where the BFS algorithm starts exploring the graph.
+   * @returns an array containing the vertices visited in breadth-first order starting from the
+   * startingVertex.
+   */
+  bfs(startingVertex){
+    const visited = new Set();
+
+    const queue = [startingVertex], result = [];
+
+    visited.add(startingVertex);
+
+
+    while(queue.length){
+      const node = queue.shift();
+      result.push(node)
+      const currVertex = this.adjacencyList.get(node);
+
+      currVertex.forEach(e => {
+        if(!visited.has(e)){
+          visited.add(e)
+          queue.push(e)
+        }
+      })
+    }
+
     return result;
   }
 
 
-  bfs(startingVertex){
-    const visited = new Set();
-    const result = [];
-    const queue = [startingVertex]
-    visited.add(startingVertex)
-
-    while(queue.length > 0){
-
-      const node = queue.shift()
-
-      const current = this.adjacencyList.get(node);
-
-      result.push(node)
-
-      current.forEach(element => {
-        if(!visited.has(element)){
-          visited.add(element)
-          queue.push(element)
-        }
-      })
-
-    }
-    return result
-
-  }
 }
 
 
+/* The code is creating a new instance of the `GraphNode` class called `graph`. It then adds vertices
+to the graph using the `addvertex` method, with values of 10, 90, 20, 50, 30, and 40. It then adds
+edges between the vertices using the `addEdges` method, creating connections between 10 and 20, 20
+and 30, 30 and 50, 50 and 90, 10 and 90, and 30 and 40. */
 const graph = new GraphNode();
 
 graph.addvertex(10)
