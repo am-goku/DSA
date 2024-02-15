f18ae7d85e16a36b4311e33b28fbab082341a61f
Gokul Krishna <amkrishna2483@gmail.com>
2024-02-15 18:01:28 +0530

    updated minHeap

--- minHeap.js
+++ minHeap.js
@@ -1,117 +1,111 @@
-class MinHeap {
-  constructor() {
+class MinHeap{
+  constructor(){
     this.heap = [];
   }
 
-  getParentIndex(index) {
-    return Math.floor((index - 1) / 2);
+  parentIndex(index){
+    return Math.floor((index-1)/2);
   }
 
-  getLeftIndex(index) {
-    return 2 * index + 1;
+  getParent(index){
+    return this.heap[this.parentIndex(index)]
   }
 
-  getRightIndex(index) {
-    return 2 * index + 2;
+  hasParent(index){
+    return this.parentIndex(index) >= 0;
   }
 
-  hasParent(index) {
-    return this.getParentIndex(index) >= 0;
+
+  leftIndex(index){
+    return 2*index+1
   }
 
-  hasLeft(index) {
-    return this.getLeftIndex(index) < this.heap.length;
+  getLeft(index){
+    return this.heap[this.leftIndex(index)]
   }
 
-  hasRight(index) {
-    return this.getRightIndex(index) < this.heap.length;
+  hasLeft(index){
+    return this.leftIndex < this.heap.length;
   }
 
-  parent(index) {
-    return this.heap[this.getParentIndex(index)];
+
+  rightIndex(index){
+    return 2*index+2
   }
 
-  left(index) {
-    return this.heap[this.getLeftIndex(index)];
+  getRight(index){
+    return this.heap[this.rightIndex(index)]
   }
 
-  right(index) {
-    return this.heap[this.getRightIndex(index)];
+  hasRight(index){
+    return this.rightIndex(index) < this.heap.length;
   }
 
-  swap(index1, index2) {
-    [this.heap[index1], this.heap[index2]] = [
-      this.heap[index2],
-      this.heap[index1],
-    ];
+  swap(i,j){
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
   }
 
-  heapifyUp() {
-    let index = this.heap.length - 1;
 
-    while (this.hasParent(index) && this.parent(index) > this.heap[index]) {
-      this.swap(index, this.getParentIndex(index));
 
-      index = this.getParentIndex(index);
-    }
-  }
-
-  heapifyDown() {
-    let index = 0;
-
-    while (this.hasLeft(index)) {
-      let minIndex = this.getLeftIndex(index);
-
-      if (this.hasRight && this.right(index) < this.heap[minIndex]) {
-        minIndex = this.getRightIndex(index);
-      }
-
-      if (this.heap[index] < this.heap[minIndex]) {
-        break;
-      } else {
-        this.swap(index, minIndex);
-
-        index = minIndex;
-      }
-    }
-  }
-
-  insert(value) {
+  insert(value){
     this.heap.push(value);
-    this.heapifyUp();
+    this.heapifyUp()
   }
 
-  extractMin() {
-    let len = this.heap.length;
 
-    if (len === 0) {
-      return null;
+  heapifyUp(){
+    let index = this.heap.length-1;
+    let parentIndex = this.parentIndex(index)
+
+    while(index > 0 && this.heap[index]<this.heap[parentIndex]){
+      this.swap(index, parentIndex);
+      index = parentIndex;
+      parentIndex = this.parentIndex(index)
+    }
+  }
+
+
+  heapifyDown(index){
+    const leftIndex = this.leftIndex(index);
+    const rightIndex = this.rightIndex(index);
+
+    let minIndex = index;
+
+    if(this.hasLeft(index) && this.heap[minIndex] > this.heap[leftIndex]){
+      minIndex = leftIndex
     }
 
-    if (len === 1) {
-      this.heap.pop();
+    if(this.hasRight && this.heap[minIndex] > this.heap[rightIndex]){
+      minIndex = rightIndex
+    }
+
+    if(minIndex === index){
       return;
     }
 
-    this.heap[0] = this.heap.pop();
-    this.heapifyDown();
+    this.swap(minIndex, index);
+
+    this.heapifyDown(minIndex)
+
   }
 
-  printHeap() {
-    const levels = Math.ceil(Math.log2(this.heap.length + 1));
-    let currentIndex = 0;
 
-    for (let i = 0; i < levels; i++) {
-      let levelString = "";
+  extractMin(){
 
-      for (let j = 0; j < 2 ** i && currentIndex < this.heap.length; j++) {
-        levelString += this.heap[currentIndex] + " ".repeat(3);
-        currentIndex++;
-      }
+    if(this.heap.length === 0) return null;
 
-      console.log(levelString);
+    const min = this.heap[0]
+    const last = this.heap.pop();
+
+    if(this.heap.length){
+      this.heap[0] = last;
+      this.heapifyDown(0);
     }
+
+    return min
   }
+
+
 }
 
 const heap = new MinHeap();
@@ -125,7 +119,5 @@ heap.insert(7);
 heap.insert(8);
 
 heap.extractMin();
-
-heap.printHeap();
 
 console.log(heap.heap);
