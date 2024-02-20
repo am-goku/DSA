5405a7422932dcede23b9447aa02df904b5dd032
Gokul Krishna <amkrishna2483@gmail.com>
2024-02-20 19:00:06 +0530

    test class

--- test.js
+++ test.js
@@ -1,23 +1,36 @@
-const sortedArray = [1, 12, 23, 34, 45, 56, 67, 78, 89, 90];
+class Parent {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
 
-const unsortedArray = [23,12,45,23,67,3,67,98,1,23,12,37,77];
 
-//////////////////////
-
-const obj = {
-    a:10,
-
-    b: {
-        x:20
+    greet(from){
+        console.log(`Happy B'Day ${this.name}, you are ${this.age} now. from ${from}`)
     }
 }
 
 
-const abc = {...obj};
+class Child extends Parent {
+    constructor(from, name, age){
+        super(name, age)
+        this.from = from;
+    }
 
 
-abc.a=100;
+    changeName (name) {
+        this.name = name
+    }
 
-abc.b.x = 200;
 
-console.log(obj)
+}
+
+
+
+const ch = new Child("goku", "joe", 26)
+
+ch.greet("goku")
+
+ch.changeName("akhil")
+
+ch.greet("goku")
